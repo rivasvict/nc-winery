102ee18ad56f47d74d1d35924b99a79ef27d8be5
Victor <vctr90w@gmail.com>
2019-11-17 16:05:59 -0400

    Added products section

--- src/landing-page/main-content/ProductShowCase.js
+++ src/landing-page/main-content/ProductShowCase.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import './ProductShowCase.scss';
 
+import productImage1 from '../../assets/products/product-image-1.png';
+
+const products = [
+  {
+    name: 'Cabernet Sauvignon',
+    image: productImage1,
+    url: '#'
+  },
+  {
+    name: 'Cabernet Sauvignon',
+    image: productImage1,
+    url: '#'
+  },
+  {
+    name: 'Cabernet Sauvignon',
+    image: productImage1,
+    url: '#'
+  },
+  {
+    name: 'Cabernet Sauvignon',
+    image: productImage1,
+    url: '#'
+  }
+];
+
 function ProductShowCase() {
+  const productsToRender = products.map((product, key) => <a href={product.url}><div key={key} id={key} className='product-item'>
+    <div className='product-image' style={{ backgroundImage: `url(${product.image})` }}>
+    </div>
+    <div className='product-caption'>
+      {product.name}
+    </div>
+  </div></a>);
+
   return (
-    <div className='product-show-case-container'>ProductShowCase</div>
+    <div className='product-show-case-container'>
+      <div className='products-title'>
+        EXPLORE / TASTE / ENJOY / FAMILY
+      </div>
+      {productsToRender}
+    </div>
   )
 }
 
