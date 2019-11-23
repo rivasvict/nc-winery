d829f05281d447c03467b6e7b15e5912c294a284
Victor <vctr90w@gmail.com>
2019-11-23 00:29:47 -0400

    Added a way to use interceptor observer to hide part of the header

--- src/landing-page/header/Header.js
+++ src/landing-page/header/Header.js
@@ -4,16 +4,45 @@ import MenuList from './MenuList';
 import Logo from './Logo';
 import Location from './location/Location';
 import './Header.scss';
+import _ from 'lodash';
 
-function Header() {
-  return (
-    <div className='header-container'>
-      <Social />
-      <Location />
-      <Logo />
-      <MenuList />
-    </div>
-  )
+function setScrollObserver(browserWindow) {
+  if (_.has(browserWindow, 'IntersectionObserver') && _.has(browserWindow, 'IntersectionObserverEntry') && 'intersectionRatio' in browserWindow.IntersectionObserverEntry.prototype) {
+    const observer = new browserWindow.IntersectionObserver(entries => {
+      const header = browserWindow.document.querySelector('.header-container');
+      if (entries[0].boundingClientRect.y < 0) {
+        header.classList.add('hidden-top-header-container');
+      } else {
+        header.classList.remove('hidden-top-header-container');
+      }
+    });
+
+    observer.observe(browserWindow.document.querySelector('html'));
+  }
+};
+
+class Header extends React.Component {
+
+  constructor(props) {
+    super();
+  }
+
+  componentDidMount() {
+    setScrollObserver(window);
+  }
+
+  render() {
+    return (
+      <div className='header-container'>
+        <div className='top-header'>
+          <Social />
+          <Location />
+        </div>
+        <Logo />
+        <MenuList />
+      </div>
+    )
+  }
 }
 
 export default Header;
