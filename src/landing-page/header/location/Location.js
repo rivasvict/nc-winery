7103551aff56f491538248dceb3c2c6adecb7801
Victor <vctr90w@gmail.com>
2019-11-24 10:49:25 -0400

    Added icons

--- src/landing-page/header/location/Location.js
+++ src/landing-page/header/location/Location.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { FaMapMarker } from 'react-icons/fa';
 
 const locationContainerStyle = {
   flex: 1,
   display: 'flex',
-  justifyContent: 'right'
+  justifyContent: 'right',
+  alignItems: 'center'
 };
 
 function Location () {
   return (
     <div style={locationContainerStyle}>
-      <span aria-label='location' role="img">📍</span> 135 Taylor Rd, Niagara-on-the-Lake, ON L0S 1J0
+      <FaMapMarker /> 135 Taylor Rd, Niagara-on-the-Lake, ON L0S 1J0
     </div>
   );
 }
